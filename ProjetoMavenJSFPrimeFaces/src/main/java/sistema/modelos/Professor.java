93e7489e14f2ae62ebe04f7a05c26b9065411a1e
160021 <160021@A2908.labinfo.facens.br>
2018-03-01 22:42:21 -0300

    Task #1 Classes ProfessorService, Professor e ProfessorManagedBeans criadas.

---
+++ ProjetoMavenJSFPrimeFaces/src/main/java/sistema/modelos/Professor.java
@@ -0,0 +1,34 @@
+package sistema.modelos;
+
+public class Professor {
+	private int professorID;
+	private String nome;
+	private String formacao;
+	
+	public int getProfessorID() {
+		return professorID;
+	}
+	
+	public void setProfessorID(int professorID) {
+		this.professorID = professorID;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getFormacao() {
+		return formacao;
+	}
+	
+	public void setFormacao(String formacao) {
+		this.formacao = formacao;
+	}
+	
+	
+	
+}
